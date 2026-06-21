8dae6d68aae8aaf7f74b38ba698f4a0255304def
Nihal Rahman <161543137+NihalRahman7@users.noreply.github.com>
2026-06-21 10:31:59 -0400

    Finish step 4
    It's the playRound() function that allows to play a single game of
    rock-paper-scissors against the computer

--- script.js
+++ script.js
@@ -27,38 +27,36 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    let message = "test";
+    let message;
 
-    switch(humanChoice) {
-        case humanChoice == "rock" && computerChoice == "paper":
-            message = "You lose! Paper beats rock";
-            computerScore++;
-            break;
-        case humanChoice == "rock" && computerChoice == "scissors":
-            message = "You win! Rock beats scissors";
-            computerScore++;
-            break;
-        case humanChoice == "paper" && computerChoice == "rock":
-            message = "You win! Paper beats rock";
-            computerScore++;
-            break;
-        case humanChoice == "paper" && computerChoice == "scissors":
-            message = "You lose! Scissors beats paper";
-            computerScore++;
-            break;
-        case humanChoice == "scissors" && computerChoice == "paper":
-            message = "You win! Scissors beats paper";
-            computerScore++;
-            break;
-        case humanChoice == "scissors" && computerChoice == "rock":
-            message = "You lose! Rock beats scissors";
-            computerScore++;
-            break;
+    if (humanChoice == "rock" && computerChoice == "paper"){
+        message = "You lose! Paper beats rock";
     }
-
+    else if (humanChoice == "rock" && computerChoice == "scissors"){
+        message = "You win! Rock beats scissors";
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock"){
+        message = "You win! Paper beats rock";
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors"){
+        message = "You lose! Scissors beats paper";
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper"){
+        message = "You win! Scissors beats paper";
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock"){
+        message = "You lose! Rock beats scissors";
+    }
+    else if (humanChoice == computerChoice) {
+        message = "It's a tie!";
+    }
+    
     return message;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
-console.log(playRound())
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+console.log(computerSelection);
+console.log(humanSelection);
+console.log(playRound(humanSelection, computerSelection))
