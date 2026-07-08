c11d22fcead62c842d2427a32676f26a8fea894d
Nihal Rahman <161543137+NihalRahman7@users.noreply.github.com>
2026-07-08 10:03:39 -0400

    Add function to show winning message

--- script.js
+++ script.js
@@ -1,5 +1,114 @@
 const MAX_SCORE = 5;
 
+let humanSelection = undefined;
+const answerContainer = document.querySelector("#answer");
+const answer = document.createElement("p");
+
+const choices = ["rock", "paper", "scissors"];
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissors = document.querySelector("#scissors");
+const buttons = document.querySelectorAll(".button");
+
+let humanScoreMsg = document.querySelector("#humanScoreMsg");
+let computerScoreMsg = document.querySelector("#computerScoreMsg");
+let humanScore = 0;
+let computerScore = 0;
+
+buttons.forEach(button => {
+    button.addEventListener("click", function(event) {
+        const clickedButton = event.currentTarget;
+        
+        if (clickedButton.textContent = "Rock"){
+            humanSelection = choices[0];
+            const computerSelection = getComputerChoice();
+            let message = playRound(humanSelection, computerSelection);
+
+            if (message.includes("tie")){
+                answer.textContent = message;
+                answerContainer.append(answer);
+            } 
+            if (message.includes("win")){
+                humanScoreMsg.textContent = `Human score: ${++humanScore}`
+                answer.textContent = message;
+                answerContainer.append(answer);
+            }    
+            if (message.includes("lose")){
+                computerScoreMsg.textContent = `Computer score: ${++computerScore}`;
+                answer.textContent = message;
+                answerContainer.append(answer);
+            }
+
+            showWinnerMsg();
+        }
+
+        else if (clickedButton.textContent = "Paper"){
+            humanSelection = choices[1];
+            const computerSelection = getComputerChoice();
+            let message = playRound(humanSelection, computerSelection);
+
+            if (message.includes("tie")){
+                answer.textContent = message;
+                answerContainer.append(answer);
+            } 
+            if (message.includes("win")){
+                humanScoreMsg.textContent = `Human score: ${++humanScore}`
+                answer.textContent = message;
+                answerContainer.append(answer);
+            }
+            if (message.includes("lose")){
+                computerScoreMsg.textContent = `Computer score: ${++computerScore}`;
+                answer.textContent = message;
+                answerContainer.append(answer);
+            }
+
+            showWinnerMsg();
+        }
+
+        else if (clickedButton.textContent = "Scissors"){
+            humanSelection = choices[2];
+            const computerSelection = getComputerChoice();
+            let message = playRound(humanSelection, computerSelection);
+
+            if (message.includes("tie")){
+                answer.textContent = message;
+                answerContainer.append(answer);
+            } 
+            if (message.includes("win")){
+                humanScoreMsg.textContent = `Human score: ${++humanScore}`
+                answer.textContent = message;
+                answerContainer.append(answer);
+            }
+            if (message.includes("lose")){
+                computerScoreMsg.textContent = `Computer score: ${++computerScore}`;
+                answer.textContent = message;
+                answerContainer.append(answer);
+            }
+        }
+
+        showWinnerMsg();
+    })
+    
+    
+})
+
+function disabledButtons(){
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+}
+
+function showWinnerMsg(){
+    if (humanScore == MAX_SCORE){
+        answer.textContent = "You won the game! Congratulations!";
+        disabledButtons();
+    }
+    if (computerScore == MAX_SCORE){
+        answer.textContent = "The computer won the game. Better luck next time.";
+        disabledButtons();
+    }
+}
+
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
     let choice = "";
@@ -44,126 +153,3 @@ function playRound(humanChoice, computerChoice) {
 
     return message;
 }
-
-
-let humanSelection = undefined;
-
-const answerContainer = document.querySelector("#answer");
-const answer = document.createElement("p");
-
-const choices = ["rock", "paper", "scissors"];
-const btnRock = document.querySelector("#rock");
-const btnPaper = document.querySelector("#paper");
-const btnScissors = document.querySelector("#scissors");
-const buttons = document.querySelectorAll(".button");
-
-let humanScoreMsg = document.querySelector("#humanScoreMsg");
-let computerScoreMsg = document.querySelector("#computerScoreMsg");
-let humanScore = 0;
-let computerScore = 0;
-
-function disabledButtons(){
-    buttons.forEach(button => {
-        button.disabled = true;
-    });
-}
-
-buttons.forEach(button => {
-    button.addEventListener("click", function(event) {
-        const clickedButton = event.currentTarget;
-        
-        if (clickedButton.textContent = "Rock"){
-            humanSelection = choices[0];
-            const computerSelection = getComputerChoice();
-            let message = playRound(humanSelection, computerSelection);
-
-            if (message.includes("tie")){
-                answer.textContent = message;
-                answerContainer.append(answer);
-            } 
-            if (message.includes("win")){
-                humanScoreMsg.textContent = `Human score: ${++humanScore}`
-                answer.textContent = message;
-                answerContainer.append(answer);
-            }    
-            if (message.includes("lose")){
-                computerScoreMsg.textContent = `Computer score: ${++computerScore}`;
-                answer.textContent = message;
-                answerContainer.append(answer);
-            }
-
-            if (humanScore == MAX_SCORE){
-                answer.textContent = "You won the game! Congratulations!";
-                disabledButtons();
-            }
-            if (computerScore == MAX_SCORE){
-                answer.textContent = "The computer won the game. Better luck next time.";
-                disabledButtons();
-            }
-        }
-
-        else if (clickedButton.textContent = "Paper"){
-            humanSelection = choices[1];
-            const computerSelection = getComputerChoice();
-            let message = playRound(humanSelection, computerSelection);
-
-            if (message.includes("tie")){
-                answer.textContent = message;
-                answerContainer.append(answer);
-            } 
-            if (message.includes("win")){
-                humanScoreMsg.textContent = `Human score: ${++humanScore}`
-                answer.textContent = message;
-                answerContainer.append(answer);
-            }
-            if (message.includes("lose")){
-                computerScoreMsg.textContent = `Computer score: ${++computerScore}`;
-                answer.textContent = message;
-                answerContainer.append(answer);
-            }
-
-            if (humanScore == MAX_SCORE){
-                answer.textContent = "You won the game! Congratulations!";
-                disabledButtons();
-            }
-            if (computerScore == MAX_SCORE){
-                answer.textContent = "The computer won the game. Better luck next time.";
-                disabledButtons();
-            }
-        }
-
-        else if (clickedButton.textContent = "Scissors"){
-            humanSelection = choices[2];
-            const computerSelection = getComputerChoice();
-            let message = playRound(humanSelection, computerSelection);
-
-            if (message.includes("tie")){
-                answer.textContent = message;
-                answerContainer.append(answer);
-            } 
-            if (message.includes("win")){
-                humanScoreMsg.textContent = `Human score: ${++humanScore}`
-                answer.textContent = message;
-                answerContainer.append(answer);
-            }
-            if (message.includes("lose")){
-                computerScoreMsg.textContent = `Computer score: ${++computerScore}`;
-                answer.textContent = message;
-                answerContainer.append(answer);
-            }
-        }
-
-        if (humanScore == MAX_SCORE){
-            answer.textContent = "You won the game! Congratulations!";
-            disabledButtons();
-        }
-        if (computerScore == MAX_SCORE){
-            answer.textContent = "The computer won the game. Better luck next time.";
-            disabledButtons();
-        }
-    })
-    
-    
-})
-
-
